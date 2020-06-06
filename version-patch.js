07895b18f321982dc95571759910f80933b9d75a
fuyu <fuyu.fluffy@gmail.com>
2020-06-06 13:21:28 +0900

    コミットハッシュを7文字に変更

--- version-patch.js
+++ version-patch.js
@@ -7,7 +7,8 @@ if (process.argv[2]) {
 	(async () => {
 		try {
 			const package = require('./package.json')
-			package.version = `${package.version}-${process.argv[2]}`
+			const suffix = process.argv[2].substring(0, 7)
+			package.version = `${package.version}-${suffix}`
 			await write('package.json', JSON.stringify(package, null, '\t'), 'utf8')
 			console.log(`version patched to ${package.version}.`)
 		} catch (e) {
